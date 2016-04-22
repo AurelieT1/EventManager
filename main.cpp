da33d0076fde738ea0db9b0d3abd92e75c6218bd
AurelieT1 <aurelie.toulet@free.fr>
2016-04-22 08:38:08 +0200

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Game.h"
+
+int main(){
+    // Program entry point.
+    Game game;
+    while(!game.GetWindow()->IsDone()){
+        game.Update();
+        game.Render();
+        game.RestartClock();
+    }
+}
